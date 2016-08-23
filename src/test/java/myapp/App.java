fc010c74318831dbd9a2d339ec5b76a31d784d94
saiteja50 <madabhushisaiteja@gmail.com>
2016-08-23 23:14:34 +0530

    Adding Files

--- src/test/java/myapp/App.java
+++ src/test/java/myapp/App.java
@@ -1,6 +1,7 @@
 class PrimeExample{  
  public static void main(String args[]){  
-  int i,m=0,flag=0;    
+  int i,m=0,flag=0; 
+  int i ,m=0,flag=0;   
   int n=17;//it is the number to be checked  
   m=n/2;    
   for(i=2;i<=m;i++){    
